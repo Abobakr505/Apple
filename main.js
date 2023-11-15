07679187be1a7369209d4d0b50f8ea33262ba6ac
bakr <abobakrhasan5335@gmail.com>
2023-11-15 10:20:49 +0200

    Apple

---
+++ main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.img');
+let contianr = document.querySelector('.contianr');
+
+function iphones (phone){
+  img.src = phone;;
+}
+
+function colors (color) {
+  contianr.style.background = color;
+}
